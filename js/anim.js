b5fca811159a5151e8d85882e6ec881858987ca3
SuguruNakahara <subaru25.net@gmail.com>
2018-10-12 15:14:29 +0900

    fix

--- js/anim.js
+++ js/anim.js
@@ -116,3 +116,9 @@ particlesJS('milkyway',
     }
   }
 );
+
+var box = document.getElementById('milkyway');
+
+box.style.top = 50 + 'px';
+box.style.left = 900 + 'px';
+box.style.transform = "rotate(30deg)";
