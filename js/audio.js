90f4f880c17a4d4605d97c2b373fc3d7277cfbbd
SuguruNakahara <subaru25.net@gmail.com>
2018-10-25 11:44:46 +0900

    main display fix

--- js/audio.js
+++ js/audio.js
@@ -1,15 +1,11 @@
 /* global AudioManager */
-
 var manager,
     canvas,
     canvasContext;
 
 window.onload = function() {
 
-//  $('#visualizer').attr('height', $('.body').height() + 300);
-
-  var isLoaded = false,
-      isClicked = false;
+ $('#visualizer').attr('height', $('.body').height() + 300);
 
   canvas = document.querySelector('canvas');
   canvasContext = canvas.getContext('2d');
@@ -20,64 +16,45 @@ window.onload = function() {
     onLoaded        : function() {
       isLoaded = true;
       this.onEnterFrame();
-      if (isClicked) {
-        this.play('bgm');
-        this.startLoop();
-      }
+      this.play('bgm');
+      this.startLoop();
     },
     onEnterFrame    : function() {
-      if (isLoaded) {
-        var me  = this,
-          dat = me.analysers.bgm.getByteFrequencyData(128),
-          len = dat.length,
-          haf = len / 2,
-          rad = 60,
-          siz = 60;
+      var me  = this,
+        dat = me.analysers.bgm.getByteFrequencyData(128),
+        len = dat.length,
+        haf = len / 2,
+        rad = 60,
+        siz = 60;
 
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-        canvasContext.strokeStyle ='#0ff';
-        
-        // canvasContext.fillStyle ='#f00';
-        canvasContext.fillStyle ='rgb(0, 255, 255)';
-        canvasContext.lineWidth = 1.0;
+      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+      canvasContext.strokeStyle ='#0ff';
+      
+      canvasContext.fillStyle ='rgb(0, 255, 255)';
+      canvasContext.lineWidth = 1.0;
 
-        canvasContext.save();
-        canvasContext.translate(canvas.width/2, canvas.height/2);
-        canvasContext.beginPath();
+      canvasContext.save();
+      canvasContext.translate(canvas.width/2, canvas.height/2);
+      canvasContext.beginPath();
 
-        // 円塗りつぶし
-        canvasContext.arc(0, 0, 58, 20, 80 * Math.PI / 180, true);
-        canvasContext.fill();
+      // 円塗りつぶし
+      canvasContext.arc(0, 0, 58, 20, 80 * Math.PI / 180, true);
+      canvasContext.fill();
 
-        for (var i = 0; i < len; i++) {
-          canvasContext.moveTo(rad+(siz*Math.floor((dat[i]/255)*100)/100),0);
-          canvasContext.lineTo(rad-1,0);
-          canvasContext.rotate(Math.PI/haf);
-        }
-
-        canvasContext.stroke();
-        canvasContext.translate(-canvas.width/2, -canvas.height/2);
-        canvasContext.restore();
+      for (var i = 0; i < len; i++) {
+        canvasContext.moveTo(rad+(siz*Math.floor((dat[i]/255)*100)/100),0);
+        canvasContext.lineTo(rad-1,0);
+        canvasContext.rotate(Math.PI/haf);
       }
+
+      canvasContext.stroke();
+      canvasContext.translate(-canvas.width/2, -canvas.height/2);
+      canvasContext.restore();
     }
   })).init();
 
   manager.load({
     bgm: '../audio/justUs.m4a'
     // bgm: '../audio/sample.mp3'
-  });
-
-  // $('#sample').click(function() {
-  $('#startViewButton').click(function() {
-    isClicked = true;
-    if (manager.isPlaying('bgm')) {
-      manager.stop('bgm');
-      manager.stopLoop();
-    } else {
-      if (isLoaded) {
-        manager.play('bgm');
-        manager.startLoop();
-      }
-    }
   });
 };
