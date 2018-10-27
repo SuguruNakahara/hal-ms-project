b6a83a5000ed1f3a72c26c99ddeabac996d66375
nobuaki hayashi <hayashi.nobuaki19@gmail.com>
2018-10-27 11:35:13 +0900

    [fix]numCheck.js main.html

--- js/numCheck.js
+++ js/numCheck.js
@@ -30,6 +30,70 @@ function createNumber(jsonData) {
     arr.push(jsonData["star-color"]);
   }
 
+  //shootingstar
+
+  if( 20 > jsonData["shooting-speed"] || jsonData["shooting-speed"] > 50){
+    arr.push(20);
+  } else {
+    arr.push(jsonData["shooting-speed"]);
+  }
+
+  if( 0 > jsonData["shooting-size"] || jsonData["shooting-size"] > 3){
+    arr.push(3);
+  } else {
+    arr.push(jsonData["shooting-size"]);
+  }
+
+  if( 3 > jsonData["shooting-opacity"] || jsonData["shooting-opacity"] > 10){
+    arr.push(6);
+  } else {
+    arr.push(jsonData["shooting-opacity"]);
+  }
+
+  if( 0 > jsonData["shooting-color"] || jsonData["shooting-color"] > 3){
+    arr.push(0);
+  } else {
+    arr.push(jsonData["shooting-color"]);
+  }
+
+  //milkyway
+
+  if( 400 > jsonData["milkyway-value"] || jsonData["milkyway-value"] > 600){
+    arr.push(500);
+  } else {
+    arr.push(jsonData["milkyway-value"]);
+  }
+  
+  if( 0 > jsonData["milkyway-color"] || jsonData["milkyway-color"] > 2){
+    arr.push(1);
+  } else {
+    arr.push(jsonData["milkyway-color"]);
+  }
+
+  if( 3 > jsonData["milkyway-size"] || jsonData["milkyway-size"] > 8){
+    arr.push(5);
+  } else {
+    arr.push(jsonData["milkyway-size"]);
+  }
+
+  if( 0.3 > jsonData["milkyway-opacity"] || jsonData["milkyway-opacity"] > 1 ){
+    arr.push(0.5);
+  } else {
+    arr.push(jsonData["milkyway-opacity"]);
+  }
+
+  if( 0 > jsonData["milkyway-left"] || jsonData["milkyway-left"] > 60){
+    arr.push(40);
+  } else {
+    arr.push(jsonData["milkyway-left"]);
+  }
+
+  if( 0 > jsonData["milkyway-rotate"] || jsonData["milkyway-rotate"] > 180){
+    arr.push(jsonData["milkyway-rotate"]);
+  } else {
+    arr.push(jsonData["milkyway-rotate"]);
+  }
+
   console.log("整形後");
   console.log(arr);
 
