8aac3512697979a232bc0f2a8445a6861fa4550d
SuguruNakahara <subaru25.net@gmail.com>
2018-11-01 23:04:35 +0900

    getQueryの切り分け

---
+++ js/getQuery.js
@@ -0,0 +1,15 @@
+function getQuery() {
+  if (1 < document.location.search.length) {
+    var query = document.location.search.substring(1);
+    var parameters = query.split('&');
+    var result = new Object();
+      for (var i = 0; i < parameters.length; i++) {
+        var element = parameters[i].split('=');
+        var paramName = decodeURIComponent(element[0]);
+        var paramValue = decodeURIComponent(element[1]);
+        result[paramName] = decodeURIComponent(paramValue);
+      }
+    return result;
+  }
+  return null;
+}
