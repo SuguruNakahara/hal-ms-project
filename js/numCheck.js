64e73080f366e1a7cb4bf4a2b80426feeb705757
SuguruNakahara <subaru25.net@gmail.com>
2018-11-07 17:00:37 +0900

    流れ星のライブラリ使用停止のうえ削除

--- js/numCheck.js
+++ js/numCheck.js
@@ -12,12 +12,6 @@ function createNumber(jsonData) {
   arr.push(fmt(jsonData["star-opacity"], 0.1, 1));
   arr.push(Math.floor(fmt(jsonData["star-color"], 0.1, 1)));
 
-  //shootingstar
-  arr.push(fmt(jsonData["shooting-speed"], 7, 14));
-  arr.push(Math.floor(fmt(jsonData["shooting-size"], 3, 5)));
-  arr.push(fmt(jsonData["shooting-opacity"], 0.3, 1));
-  arr.push(Math.floor(fmt(jsonData["shooting-color"], 0, 2)));
-
   //milkyway
   arr.push(Math.floor(fmt(jsonData["milkyway-value"], 100, 400)));
   arr.push(Math.floor(fmt(jsonData["milkyway-color"], 0, 2)));
