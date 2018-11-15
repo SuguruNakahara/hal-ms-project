2120c847983bae1155635074f878b5839491fa7e
kobayashiAzuki <kobayashi.y222@gmail.com>
2018-11-15 14:49:45 +0900

    流れ星・オープニング修正

--- js/shootingStar.js
+++ js/shootingStar.js
@@ -16,26 +16,26 @@ function shootingStar_style(speed,angle) {
 
   var styleSheet = document.styleSheets[1];
   var keyframes = "@keyframes kirakira {0% {transform:translateY(0) rotateY(0)} 100% {transform:translate("+ angle2 +"vh,calc(100vh + 1em)) rotateZ(1800deg)}}";
-  
+
   styleSheet.deleteRule(styleSheet.cssRules.length-1);
+  console.log(angle2);
   styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
-  
 };
 
-function starMaker(n, setColor,angle) {
+function starMaker(setColor,angle,speed) {
   const star = document.getElementById('shootingStar');
   star.style.color = setColor;
-  for(let i = 0; i < n; i++) {
-    starSet(star,angle);
+  for(let i = 0; i < 1; i++) {
+    starSet(star,angle,speed);
   }
 }
 
-function starSet(clone,an) {
+function starSet(clone,an,speed) {
   const starClone = clone.cloneNode(true);
   const starStyle = starClone.style;
   starStyle.left = 100 * Math.random() + "%";
   starStyle.animationDelay = 6 * Math.random() + "s";
-  shootingStar_style(5,an);
+  shootingStar_style(speed,an);
 
   document.body.appendChild(starClone);
 
