0986d868ca66844f5b09aee71b3167680f9f29b4
SuguruNakahara <subaru25.net@gmail.com>
2018-11-22 15:07:23 +0900

    opening調整

--- js/opening.js
+++ js/opening.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',() =>{
   const roll = document.getElementById('roll');
   const distance = window.innerHeight + roll.clientHeight
-  const duration = distance / 39;
+  const duration = distance / 100;
   
   roll.style.animationDuration = `${duration}s`
   roll.classList.add('anim');
